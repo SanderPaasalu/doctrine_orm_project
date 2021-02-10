20d87c9f47d7d3af6de61fc23f50a3295a5ab023
Sander.Paasalu <sander.paasalu@khk.ee>
2021-02-10 13:50:08 +0200

    Loetud ja kuvatud yhe artikli andmed repositooriumi abil

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,13 +4,29 @@ namespace App\Controller;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exeption\HttpNotFoundException;
 
 class ArticleController extends Controller
 {
-    public function view(Request $request, Response $response)
+    public function view(Request $request, Response $response, $args = [])
     {
-    	$article = $this->ci->get('db')->find('App\Entity\Article', 1); 
+    	$article = $this->ci->get('db')->getRepository('App\Entity\Article')->findOneBy([
+    		'slug' => $args['slug']
+    	]); 
+
+    	if (!$article){
+    		throw new HttpNotFoundException($request);
+    	}
+    	
         return $this->renderPage($response, 'article.html', [
+        	'article' => $article
+        ]);
+    }
+
+    public function viewPK(Request $request, Response $response) 
+    {
+    	$article = $this->ci->get('db')->find('App\Entity\Article', 1);
+    	return $this->renderPage($response, 'article.html', [
         	'article' => $article
         ]);
     }
