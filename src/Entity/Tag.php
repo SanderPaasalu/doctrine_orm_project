f8775f28299b5661e1506ab437b8dc0381ed37db
Sander.Paasalu <sander.paasalu@khk.ee>
2021-03-01 09:07:19 +0200

    Koostatud tag.php

---
+++ src/Entity/Tag.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+* @Entity
+*/
+class Tag {
+
+	/**
+	* @Column(type="integer")
+	* @Id
+	* @GeneratedValue
+	*/
+	private $id;
+
+	/**
+	* @Column(type="string")
+	*/
+	private $name;
+
+	public function getId(){
+		return $this->id;
+	}
+	public function setId($value){
+		$this->id = $value;
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+	public function setName($value){
+		$this->name = $value;
+	}
+}
